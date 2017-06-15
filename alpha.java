c842d44be4876b3e527ce0ff38c1967c5e7647be
maathangi96 <maathangi96@gmail.com>
2017-06-15 12:04:15 +0530

    alpha.java

---
+++ alpha.java
@@ -0,0 +1,16 @@
+import.java.io.*;
+import java.util.Scanner;
+class alpha
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+char ch=s.next().charAt(0);
+if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+{
+System.out.println("vowel");
+}
+else
+System.out.println("consonant");
+}
+}
